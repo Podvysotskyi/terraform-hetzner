1f4bcb80efcec8d84bdd32dcd92761690e5239bf
Serhii Podvysotskyi <spodvysotskyi@quavermusic.com>
2025-07-28 15:46:44 -0500

    format

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 terraform {
-    backend "s3" {
-        bucket   = "podvysotskyi-github"
-        endpoint = "fsn1.your-objectstorage.com"
-        key      = "terraform-hetzner/terraform.tfstate"
-        region   = "main"
+  backend "s3" {
+    bucket   = "podvysotskyi-github"
+    endpoint = "fsn1.your-objectstorage.com"
+    key      = "terraform-hetzner/terraform.tfstate"
+    region   = "main"
 
-        skip_credentials_validation = true
-        skip_metadata_api_check     = true
-        skip_region_validation      = true
-    }
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+  }
 }
