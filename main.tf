e3451593de1140c6603390fd91678d1901004104
Serhii Podvysotskyi <spodvysotskyi@quavermusic.com>
2025-07-28 15:33:39 -0500

    remove vars

--- main.tf
+++ main.tf
@@ -1,18 +1,12 @@
 terraform {
     backend "s3" {
-        bucket   = var.S3_BUCKET
-        endpoint = var.S3_ENDPOINT
-        key      = var.S3_TF_STATE
-        region   = var.S3_REGION
+        bucket   = "podvysotskyi-github"
+        endpoint = "fsn1.your-objectstorage.com"
+        key      = "terraform-hetzner/terraform.tfstate"
+        region   = "main"
 
         skip_credentials_validation = true
         skip_metadata_api_check     = true
         skip_region_validation      = true
-    }
-}
-
-resource "null_resource" "example" {
-    triggers = {
-        value = "A example resource that does nothing!"
     }
 }
