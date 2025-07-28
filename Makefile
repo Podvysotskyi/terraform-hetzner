ce4b7b6c089445e6a21a3697801d76cea419071f
Serhii Podvysotskyi <spodvysotskyi@quavermusic.com>
2025-07-28 16:49:54 -0500

    update key

--- Makefile
+++ Makefile
@@ -1,5 +1,4 @@
 # Makefile
-
 SHELL := /bin/bash
 
 init:
--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket   = "podvysotskyi-github"
     endpoint = "https://fsn1.your-objectstorage.com"
-    key      = "terraform.tfstate"
+    key      = "terraform-hetzner/terraform.tfstate"
 
     region                      = "main" # this is required, but will be skipped!
     skip_credentials_validation = true   # this will skip AWS related validation
