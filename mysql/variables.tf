996b72491769b4fc184ec23f277ae8db5c3ad58d
Serhii Podvysotskyi <podvysotskyi@hotmail.com>
2025-08-05 16:40:07 -0500

    add postgres

--- mysql/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "docker_host" {
-  type      = string
-  sensitive = true
-}
-
-variable "mysql_root_password" {
-  type      = string
-  sensitive = true
-}
---
+++ traefik.tf
@@ -0,0 +1,40 @@
+module "traefik" {
+  source = "./modules/deployable/docker/traefik"
+
+  docker = {
+    host = "ssh://${var.HETZNER_USER}@${var.HETZNER_IP}:${var.HETZNER_SSH_PORT}"
+  }
+
+  server = {
+    hostname = var.CLOUDFLARE_DOMAIN
+  }
+
+  cloudflare = {
+    email = var.CLOUDFLARE_EMAIL
+    token = var.CLOUDFLARE_TOKEN
+  }
+
+  tcp_ports = [
+    {
+      port = module.mysql.port
+    },
+    {
+      port = module.postgres.port
+    }
+  ]
+}
+
+module "traefik_cloudflare_record" {
+  source = "./modules/common/cloudflare/dns-record"
+
+  cloudflare = {
+    email  = var.CLOUDFLARE_EMAIL
+    token  = var.CLOUDFLARE_TOKEN
+    domain = var.CLOUDFLARE_DOMAIN
+  }
+
+  record = {
+    ip   = var.HETZNER_IP
+    name = module.traefik.host
+  }
+}
